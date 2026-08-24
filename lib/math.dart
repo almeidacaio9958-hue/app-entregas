1794b35d20d37e9ff8f355c5bd395e771f1f067f
almeidacaio9958-hue <almeidacaio9958@gmail.com>
2026-08-24 04:42:44 -0300

    Add math library with trigonometric and sqrt functions

---
+++ lib/math.dart
@@ -0,0 +1,7 @@
+library math;
+import 'dart:math' as dart_math;
+
+double sin(double radians) => dart_math.sin(radians);
+double cos(double radians) => dart_math.cos(radians);
+double asin(double radians) => dart_math.asin(radians);
+double sqrt(double number) => dart_math.sqrt(number);
